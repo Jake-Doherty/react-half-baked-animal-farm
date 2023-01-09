9f820894326b2dfa3534fbf9f4b1c47f03a87120
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-09 13:17:23 -0800

    add elements w/ props to animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <h1 className="animal name">{props.name}</h1>
+      <p>{props.type}</p>
+      <p>{props.says}</p>
+    </div>
+  );
 }
