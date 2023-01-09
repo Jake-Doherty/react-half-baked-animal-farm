0bcc7b33119b1ed8874f48b6d682a682488a0fc2
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-09 14:26:02 -0800

    utilize provided classes in animal.css correctly

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,7 +4,7 @@ import './Animal.css';
 export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
-      <h2 className="animal name">{props.name}</h2>
+      <h2 className="name">{props.name}</h2>
       <img src={`/animals/${props.type}.svg`} />
       <p>{props.says}</p>
     </div>
